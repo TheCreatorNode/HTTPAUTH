4c5f26b2e68b13179282a4d6ccbb0293f0358d28
TheCreatorNode <basseyu01@gmail.com>
2025-11-02 18:46:08 +0100

    FIX: bug in GET profile route

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const server = http
     const { method, url, headers } = req;
     const fullURL = new URL(url, `http://${headers.host}`);
     const path = fullURL.pathname;
+    console.log(path);
     let usersData;
 
     if (method === "POST" && path === "/signup") {
@@ -73,7 +74,7 @@ const server = http
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({ Message: "Login successful", token }));
       });
-    } else if (req === "GET" && path === "/profile") {
+    } else if (method === "GET" && path === "/profile") {
       const auth = headers["authorization"];
 
       if (!auth || !auth.startsWith("Bearer")) {
@@ -83,6 +84,7 @@ const server = http
       }
 
       const token = auth.split(" ")[1];
+      console.log(token);
       usersData = readFile();
       const user = usersData.find((u) => (u.token = token));
 
@@ -98,107 +100,4 @@ const server = http
       res.end(JSON.stringify({ error: "Route not found" }));
     }
   })
-  .listen(3000);
-
-// const http = require("http");
-// const fs = require("fs");
-// const crypto = require("crypto");
-
-// const PORT = 3000;
-// const USERS_FILE = "./users.json";
-
-// // helper to read and write user data
-// function readUsers() {
-//   return JSON.parse(fs.readFileSync(USERS_FILE, "utf8"));
-// }
-
-// function writeUsers(users) {
-//   fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
-// }
-
-// // helper to hash passwords
-// function hashPassword(password, salt = crypto.randomBytes(16).toString("hex")) {
-//   const hashed = crypto
-//     .pbkdf2Sync(password, salt, 1000, 64, "sha512")
-//     .toString("hex");
-//   return { salt, hashed };
-// }
-
-// // helper to verify password
-// function verifyPassword(password, user) {
-//   const hashed = crypto
-//     .pbkdf2Sync(password, user.salt, 1000, 64, "sha512")
-//     .toString("hex");
-//   return hashed === user.hashed;
-// }
-
-// // helper to send JSON response
-// function send(res, status, data) {
-//   res.writeHead(status, { "Content-Type": "application/json" });
-//   res.end(JSON.stringify(data));
-// }
-
-// // main server
-// const server = http.createServer((req, res) => {
-//   if (req.method === "POST" && req.url === "/signup") {
-//     let body = "";
-//     req.on("data", (chunk) => (body += chunk));
-//     req.on("end", () => {
-//       const { username, password } = JSON.parse(body);
-//       if (!username || !password)
-//         return send(res, 400, { error: "Missing fields" });
-
-//       const users = readUsers();
-//       if (users.find((u) => u.username === username)) {
-//         return send(res, 400, { error: "User already exists" });
-//       }
-
-//       const { salt, hashed } = hashPassword(password);
-//       const user = { username, salt, hashed };
-//       users.push(user);
-//       writeUsers(users);
-//       send(res, 201, { message: "Signup successful!" });
-//     });
-//   } else if (req.method === "POST" && req.url === "/login") {
-//     let body = "";
-//     req.on("data", (chunk) => (body += chunk));
-//     req.on("end", () => {
-//       const { username, password } = JSON.parse(body);
-//       const users = readUsers();
-//       const user = users.find((u) => u.username === username);
-
-//       if (!user || !verifyPassword(password, user)) {
-//         return send(res, 401, { error: "Invalid credentials" });
-//       }
-
-//       const token = Math.random().toString(36).substring(2);
-//       user.token = token;
-//       writeUsers(users);
-//       send(res, 200, { message: "Login successful", token });
-//     });
-//   } else if (req.method === "GET" && req.url === "/profile") {
-//     const auth = req.headers["authorization"];
-//     if (!auth || !auth.startsWith("Bearer ")) {
-//       return send(res, 401, {
-//         error: "Missing or invalid Authorization header",
-//       });
-//     }
-
-//     const token = auth.split(" ")[1];
-//     const users = readUsers();
-//     const user = users.find((u) => u.token === token);
-
-//     if (!user) return send(res, 403, { error: "Invalid token" });
-
-//     send(res, 200, {
-//       username: user.username,
-//       message: "Welcome to your profile!",
-//     });
-//   } else {
-//     send(res, 404, { error: "Route not found" });
-//   }
-// });
-
-// server.listen(PORT, () =>
-//   console.log(` Server running on http://localhost:${PORT}`)
-// );
+  .listen(3000, () => console.log(` Server running on http://localhost:3000`));
